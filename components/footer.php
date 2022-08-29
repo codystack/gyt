184ab0f44366fdf66a688fe46868965a68f6c992
ThankGod Okoro <thankgodokoro@ThankGods-MacBook-Pro.local>
2022-08-29 06:41:48 +0100

    index page added

---
+++ components/footer.php
@@ -0,0 +1,23 @@
+    <!-- footer -->
+    <footer class="py-7 py-xl-7 bg-black inverted">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center mb-3">
+                    <a href="./" class="footer-brand">
+                        <img src="assets/images/footericon.svg" alt="Logo">
+                    </a>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <p>&copy; Copyrights <script>document.write(new Date().getFullYear());</script> All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+  <script src="https://cube.webuildthemes.com/assets/js/vendor.bundle.js"></script>
+  <script src="https://cube.webuildthemes.com/assets/js/index.bundle.js"></script>
+
+</body>
+
+</html>
---
+++ components/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon" />
+  <link rel="stylesheet" href="assets/css/libs.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <title>Fore Thought Consult&trade;</title>
+  
+</head>
+
+<body>
