ac93028f9375f5a53f96341c54cdfc927b44a5b1
ThankGod Okoro <thankgodokoro@ThankGods-MacBook-Pro.local>
2022-08-31 15:25:01 +0100

    index page modified

--- components/navbar.php
+++ components/navbar.php
@@ -3,18 +3,18 @@
     <div class="container">
       <a href="./" class="navbar-brand"><img src="assets/images/logo_white.svg" alt="Logo"></a>
   
-    <!-- secondary -->
-    <ul class="navbar-nav navbar-nav-secondary order-lg-3">
-        <li class="nav-item d-lg-none">
-          <a class="nav-link nav-icon" href="" role="button" data-bs-toggle="collapse" data-bs-target="#navbar"
-            aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="bi bi-list"></span>
-          </a>
-        </li>
-        <li class="nav-item d-none d-lg-block">
-          <a href="get-started" class="btn btn-outline-white rounded-pill ms-2"> Get Started </a>
-        </li>
-    </ul>
+      <!-- secondary -->
+      <ul class="navbar-nav navbar-nav-secondary order-lg-3">
+          <li class="nav-item d-lg-none">
+            <a class="nav-link nav-icon" href="" role="button" data-bs-toggle="collapse" data-bs-target="#navbar"
+              aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="bi bi-list"></span>
+            </a>
+          </li>
+          <li class="nav-item d-none d-lg-block">
+            <a href="get-started" class="btn btn-outline-white rounded-pill ms-2"> Get Started </a>
+          </li>
+      </ul>
   
       <!-- primary -->
       <div class="collapse navbar-collapse" id="navbar" data-bs-parent="#mainNav">
@@ -23,10 +23,7 @@
             <a class="nav-link" href="./"> Home </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="about"> About Us </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="terms"> Terms </a>
+            <a class="nav-link" href="#about"> About Us </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="contact"> Contact Us </a>
---
+++ contact.php
@@ -0,0 +1,43 @@
+<?php 
+$page = "Contact";
+include "./components/header.php";
+include "./components/navbardark.php";
+?>
+
+
+
+  
+  
+  
+
+
+  <section class="py-15 py-xl-20">
+    <div class="container mt-5 mt-lg-10">
+      <div class="row align-items-center justify-content-between">
+        <div class="col-lg-6 mb-4 mb-lg-0">
+          <h1>Interested in working together? Contact!</h1>
+          <p>For any enquiries, or to say hello👋,<br>get in touch with us to see how we can help you with your project.</p>
+          <hr class="my-4 fw-25 ml-0">
+          <ul class="list-group list-group-minimal">
+            <li class="list-group-item d-flex align-items-center">
+              <span class="w-25 text-muted">Email</span>
+              info@forethoughtconsultgyt.com
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-5">
+          <div class="media equal-1-1">
+            <div id="map1" class="map"></div>
+          </div>
+          <div class="card bg-black text-white">
+            <div class="card-body">
+              <h5>ORUÇREİS MAH. TEKSTİLKENT CAD. A BLOK NO: 12 B İÇ KAPI NO: 011 ESENLER/ İSTANBUL</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<?php include "./components/footer.php";?>
